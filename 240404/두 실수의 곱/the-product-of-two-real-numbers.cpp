b10e32ff9cdae925a27517a38fe8bcccd92a48d8
Bong Channavong <55526740+Navong@users.noreply.github.com>
2024-04-04 17:30:53 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a = 5.26;
+    float b = 8.27;
+    cout << fixed << setprecision(3) << a * b;
+    return 0;
+}
