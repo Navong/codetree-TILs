abd64659cc0c2b482c0fec3e7520638ba5ac699c
Bong Channavong <55526740+Navong@users.noreply.github.com>
2024-04-05 14:30:15 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240405/Date to Date/date-to-date.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Please write your code here.
+    int month, day, till_month, till_day;
+    int elapse_day = 0;
+    cin >> month >> day >> till_month >> till_day;
+    int day_of_month[13] = {0, 31, 28,31,30,31,30,31,30,31,30,31,30};
+    while(true){
+        if (month == till_month && day == till_day)
+            break;
+        
+        day++;
+        elapse_day++;
+        if (day == day_of_month[month]){
+            month++;
+            day = 0;
+        }
+    }
+
+    cout << elapse_day;
+    return 0;
+}
