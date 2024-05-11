39cf78a96ee6d624576e5e2f06cb02559bdb433d
Bong Channavong <55526740+Navong@users.noreply.github.com>
2024-05-11 16:46:14 +0900

    Title: 최고의 33위치, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240511/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+grid =[
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+
+def all_gold(row_s, col_s, row_e, col_e):
+    num_of_gold = 0
+
+    for row in range(row_s, row_e + 1): # by adding 1 to the row and col to make sure that the last cell of subgrid is included
+        for col in range(col_s, col_e + 1):
+            num_of_gold += grid[row][col]
+
+    return num_of_gold
+
+#explore all cases in left corner of the grid 
+max_gold = 0
+
+for row in range(n):
+    for col in range(n):
+        #if the 1 3 grid goes beyond the n n grid
+        if row + 2 >= n or col + 2 >= n:
+            continue
+
+        max_gold = all_gold(row, col, row + 2, col + 2)
+
+print(max_gold)
