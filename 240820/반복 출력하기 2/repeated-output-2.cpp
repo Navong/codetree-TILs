2464268eea291b96916331caf7e74bc201c0bef7
Bong Channavong <55526740+Navong@users.noreply.github.com>
2024-08-20 15:14:03 +0900

    Title: 반복 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void PrintStar(int n){
+    if(n==0) return;
+
+    PrintStar(n-1);
+    cout <<"HelloWorld";
+
+
+    cout << endl;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    PrintStar(n);
+    return 0;
+}
