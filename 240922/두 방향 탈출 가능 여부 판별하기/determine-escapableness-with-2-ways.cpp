d37e9d96e1dcbce07ccb214c9a32ff89192b3da8
Navong <55526740+Navong@users.noreply.github.com>
2024-09-22 00:04:12 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240922/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//direction
+int dx[] = {1,0};
+int dy[] = {0,1};
+
+vector<vector<bool>> marked;
+
+bool isValid(vector<vector<int>>& grid, int x, int y, int n, int m){
+    return (x>=0&&x<n&&y>=0&&y<m&&grid[x][y]==1&&!marked[x][y]);
+}
+
+void dfs(vector<vector<int>>& grid, int x, int y, int n, int m){
+    marked[x][y]=true;
+    for(int i=0;i<2;i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(isValid(grid, nx,ny,n,m)){
+            dfs(grid,nx,ny,n,m);
+        }
+    }
+}
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+
+    //grid vector
+    vector<vector<int>> grid(n, vector<int>(m));
+    marked = vector<vector<bool>>(n, vector<bool>(m,false));
+
+    //grid input
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> grid[i][j];
+        }
+    }
+
+    if(marked[0][0]==0||marked[n-1][m-1]==0){
+        cout << 0 << endl;
+        return 0;
+    }
+
+    dfs(grid, 0, 0, n,m);
+
+    if(marked[n-1][m-1]==1){
+        cout<<1<<endl;
+    }else cout << 0 << endl;
+
+    return 0;
+}
