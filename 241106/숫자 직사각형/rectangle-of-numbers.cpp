c86e2af7500e2346ae9f820af7e4d048c5e212c9
Navong <55526740+Navong@users.noreply.github.com>
2024-11-06 11:57:54 +0900

    Title: 숫자 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/숫자 직사각형/rectangle-of-numbers.cpp
+++ 241106/숫자 직사각형/rectangle-of-numbers.cpp
@@ -3,25 +3,28 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int row, col;
-    int arr[100][100];
-    cin >> row >> col;
+    int n,m;
+    int grid[100][100];
 
+    cin >> n >> m;
+
+    // input grid
     int num = 1;
 
-    for(int i = 0; i < row; i++){
-        for(int j=0; j < col; j++){
-            arr[i][j] = num++;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            grid[i][j] = num++;
         }
     }
 
-    for(int i = 0; i < row; i++){
-        for(int j=0; j < col; j++){
-            cout << arr[i][j] << ' ';
+    //print grid
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout << grid[i][j] << " ";
         }
 
         cout << endl;
     }
-
     return 0;
 }
